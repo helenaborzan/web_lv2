8b6c3b8266bf1f58987aa02c61e877ba112b7a2d
cvitkovi.roko5 <cvitkovi.roko5@gmail.com>
2025-04-08 12:26:54 +0200

    Add project from lv1

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.static('public')); // "posluzuje" index.html
+// Automatski koristi sve iz mape public
 app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
+res.send("Ili obican tekst ako nema HTML datoteke.");
 });
-app.listen(PORT, () => {
-console.log(`Server pokrenut na portu ${PORT}`);
+app.listen(3000, () => {
+console.log("Server pokrenut na http://localhost:3000");
 });
